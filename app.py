fc425d0fc3a123f06d9b0b1a727198aefd62a4d5
Aratunde Temitayo <35546618+a-tayo@users.noreply.github.com>
2022-06-09 05:07:30 +0100

    Renamed __init__ to app This was done due to failure of flask migrate to init the db.

---
+++ app.py
@@ -0,0 +1,42 @@
+# imports
+from flask import jsonify, request
+from flask_cors import CORS
+from models import db, app, Plants
+
+# initialization
+db.init_app(app)
+cors = CORS(app)
+
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    response.headers.add('Access-Control-Allow-Headers', 'GET, POST, DELETE, OPTIONS, PATCH')
+    return response
+
+
+@app.route('/plants', methods=['GET', 'POST'])
+def get_plants():
+    # implement pagination to display only 10 results at a time
+    page = request.args.get('page', 1, type=int)
+    start = (page -1)*10
+    end = start+10
+
+    # query the plants db to get all plants entries in the db
+    plants = Plants.query.all()
+
+    # calls the format method of the Plants class
+    formatted_plants = [plant.format for plant in plants]
+    return jsonify({
+        'success': True,
+        'pages': formatted_plants[start:end],
+        'results': len(formatted_plants)
+    })
+
+@app.route('/smiley')
+def smiley():
+    return ':)'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.Environment = 'Development'
+    app.run()
